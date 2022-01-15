4701098c7dbc8a0e8e01b34611f0adce36503e92
xhegithub <97580949+xhegithub@users.noreply.github.com>
2022-01-15 09:24:02 -0800

    change styles

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ function next() {
 function go(dtStr, removeCurrentDate) {
     if(dtStr){
         document.getElementById("currentDay").innerHTML = dtStr.replace('=', '');
-        document.title = "基督之家第五家QT讀經靈修 - " + dtStr;
-        document.getElementById("iframeTag").src = "https://hocdevotion.org/event/" + dtStr;
+        document.title = "基督之家第五家QT讀經靈修 - " + dtStr.replace('=', '');
+        document.getElementById("iframeTag").src = "https://hocdevotion.org/event/" + dtStr +'/?variant=zh-cn';
     }else{
         var currentUrl = window.location.href;
         var current_time = new Date().getTime();
@@ -35,14 +35,14 @@ function go(dtStr, removeCurrentDate) {
         var month = dt.getMonth() + 1;
         var day = dt.getDate();
         var dtStr = yyyy + '-' + month + "-" + day;
-        document.getElementById("currentDay").innerHTML = dtStr;
+        document.getElementById("currentDay").innerHTML = dtStr.replace('=', '');
         //document.getElementById("iframeTag").src = "https://hocdevotion.org/event/" + dtStr;
-        document.title = "基督之家第五家QT讀經靈修 - " + dtStr;
+        document.title = "基督之家第五家QT讀經靈修 - " + dtStr.replace('=', '');;
 
         if(removeCurrentDate && currentUrl.includes("?")){
             currentUrl = currentUrl.split("?")[0];
         }
-        window.location.href = currentUrl +'?'+dtStr;
+        window.location.href = currentUrl +'?'+dtStr+'/?variant=zh-cn';
     }
 }
 
